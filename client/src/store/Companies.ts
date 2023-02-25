627de0550b0a958cd430bbcc02a99991e4673f9a
propertiess <erofeev2001@yandex.ru>
2023-02-25 10:57:26 +0300

    feat: edit store methods

--- client/src/store/Companies.ts
+++ client/src/store/Companies.ts
@@ -1,24 +1,64 @@
 import { makeAutoObservable } from 'mobx';
 
+import { PredictService } from '@/services';
 import { TCompanies } from '@/types';
 
 class Companies {
   byMA: TCompanies = [];
   byLR: TCompanies = [];
+  isLoading = false;
+  error: Error | null = null;
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  setByMovingAverage(companies: TCompanies) {
-    this.byMA = companies;
+  async getPredictByMovingAverage(formData: FormData) {
+    this.setIsLoading(true);
+    this.setError(null);
+    try {
+      const response = await PredictService.getPredictByMovingAverage(formData);
+      this.setByMA(response);
+    } catch (e) {
+      if (e instanceof Error) {
+        this.setError(e);
+      }
+    }
+    this.setIsLoading(false);
   }
 
-  setByLinearRegression(companies: TCompanies) {
+  async getPredictByLinearRegression(formData: FormData) {
+    this.setIsLoading(true);
+    this.setError(null);
+
+    try {
+      const response = await PredictService.getPredictByMovingAverage(formData);
+      this.setByLR(response);
+    } catch (e) {
+      if (e instanceof Error) {
+        this.setError(e);
+      }
+    }
+    this.setIsLoading(false);
+  }
+
+  private setIsLoading(loading: boolean) {
+    this.isLoading = loading;
+  }
+
+  private setError(error: Error | null) {
+    this.error = error;
+  }
+
+  private setByLR(companies: TCompanies) {
     this.byLR = companies;
   }
 
-  clearAll() {
+  private setByMA(companies: TCompanies) {
+    this.byMA = companies;
+  }
+
+  clearCompanies() {
     this.byLR = [];
     this.byMA = [];
   }
