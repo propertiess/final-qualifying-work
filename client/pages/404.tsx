0f1ec5bc2fefd5992885c649a76386266e7ea6c3
propertiess <erofeev2001@yandex.ru>
2023-02-28 23:04:30 +0300

    feat: add 404 page

---
+++ client/pages/404.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '@/layout';
+
+const ErrorPage = () => {
+  return (
+    <Layout title='Страница не найдена!' description='404'>
+      Страница не найдена!
+    </Layout>
+  );
+};
+
+export default ErrorPage;
